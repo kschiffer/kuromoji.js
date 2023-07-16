0c82f95c981821729f350dd4d10cde33450f8fd5
Kevin Schiffer <mail@kevinschiffer.com>
2023-07-16 13:31:25 +0000

    Bundle with webpack

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+// Generated using webpack-cli https://github.com/webpack/webpack-cli
+
+const path = require('path');
+
+const isProduction = process.env.NODE_ENV == 'production';
+
+const config = {
+    entry: {
+        kuromoji: './src/kuromoji.js',
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'build'),
+        libraryTarget: 'commonjs2',
+    },
+    resolve: {
+        fallback: { path: require.resolve('path-browserify')},
+    },
+    plugins: [
+        // Add your plugins here
+        // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/i,
+                loader: 'babel-loader',
+            },
+            {
+                test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+                type: 'asset',
+            },
+
+            // Add your rules for custom modules here
+            // Learn more about loaders from https://webpack.js.org/loaders/
+        ],
+    },
+};
+
+module.exports = () => {
+    if (isProduction) {
+        config.mode = 'production';
+        
+        
+    } else {
+        config.mode = 'development';
+    }
+    return config;
+};
